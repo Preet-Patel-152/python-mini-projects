9351d5717f94d69691d998de28e3b884c9014a49
patel-preet5 <77303327+patel-preet5@users.noreply.github.com>
2025-12-09 23:16:32 -0500

    updated guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,4 @@
+# quick guessing game program
 import random
 
 ran_num = random.randint(1, 50)
@@ -6,11 +7,15 @@ print(
     "welcome to the guessing game! I have selected a number between 1 and 50. Can you guess it?"
 )
 user_num = int(input("Enter your guess: "))
+attemp = 1
 while user_num != ran_num:
     if user_num < ran_num:
         print("Too low! Try again.")
+        attemp += 1
     else:
         print("Too high! Try again.")
+        attemp += 1
     user_num = int(input("Enter your guess: "))
-print("Congratulations! You've guessed the correct number:", ran_num)
+print("Congratulations! You've guessed the correct number:",
+      ran_num, "in", attemp, "attempts.")
 print("THANK YOU FOR PLAYING THE GUESSING GAME")
