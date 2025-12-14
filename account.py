9d99cfd58a3b2efd8b22da35fad1785d0e5ff532
Preet-Patel-152 <preetpatel7@hotmail.com>
2025-12-14 17:58:25 -0500

    class

---
+++ account.py
@@ -0,0 +1,4 @@
+class bank:
+    def __init__(self, account_holder={}, initial_balance=0):
+        self.account_holder = account_holder
+        self.balance = initial_balance
---
+++ bank_account.py
@@ -0,0 +1,5 @@
+from account import bank
+
+bacnk1 = bank({"Alice": "123456"}, 1000)
+
+print(bacnk1.account_holder)
