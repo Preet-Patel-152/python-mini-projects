7e062d53cefe6652c3cd24e62f39f4d6a929727e
Preet-Patel-152 <preetpatel7@hotmail.com>
2025-12-15 17:29:52 -0500

    added new options

--- bank_account.py
+++ bank_account.py
@@ -59,6 +59,40 @@ while is_running:
                         new_password = input("Enter new password: ").strip()
                         print(bank_acc.change_acccout_password(new_password))
 
+                    case "transfer":
+                        recipient_name = input(
+                            "Enter recipient account holder name: ").strip()
+                        accounts_recipient = accounts.get(recipient_name)
+                        if accounts_recipient:
+                            amount = float(input("Enter amount to transfer: "))
+                            if amount > 0 and amount <= bank_acc.balance:
+                                bank_acc.withdraw(amount)
+                                accounts_recipient.deposit(amount)
+                                print(
+                                    f"Transferred ${amount} to {recipient_name}.")
+                            else:
+                                print("Insufficient funds or invalid amount.")
+                    case "developer":
+                        print("Developer mode activated.")
+                        print('        _   ,_,   _')
+                        print('       / `\'=) (=\'` \\')
+                        print('      /.-.-.\\ /.-.-.\\ ')
+                        print('      `      "      `')
+
+                        while True:
+                            dev_action = input(
+                                "what would you like to d ocheck number of accounts or quit? ").strip().lower()
+                            match dev_action:
+                                case "check number of accounts":
+                                    print(
+                                        f"Total bank accounts: {bank.num_bank_acc}")
+                                case "quit":
+                                    print("Exiting developer mode.")
+                                    break
+                                case _:
+                                    print(
+                                        "Invalid developer action. Please try again.")
+
                     case "exit":
                         print("Exiting account management.")
                         break
